1c22294897d71aa6db9219c63bea374ac9b391d9
Prykval <pryvalera21@gmail.com>
2024-02-11 19:52:04 +0100

    3

--- front-init/main.py
+++ front-init/main.py
@@ -1,13 +1,18 @@
 import urllib.parse
+import logging
 import json
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from pathlib import Path
 import mimetypes
-from jinja2 import Environment, FileSystemLoader
+from threading import Thread
+import socket
 
-
+BUFFER_SIZE = 1024
 BASE_DIR = Path()
-jinja= Environment(loader=FileSystemLoader('templates'))
+HTTP_PORT = 3000
+HTTP_HOST = '0.0.0.0'
+SOCKET_HOST = '127.0.0.1'
+SOCKET_PORT = 5000
 
 class Homeworkfork(BaseHTTPRequestHandler):
 
@@ -19,7 +24,7 @@ class Homeworkfork(BaseHTTPRequestHandler):
             case '/Home':
                 self.send_html('Home.html')
             case '/Send message':
-                self.render_template('message.html')
+                self.send_html('message.html')
             case _:
                 file = BASE_DIR.joinpath(route.path[1:])
                 if file.exists():
@@ -31,7 +36,15 @@ class Homeworkfork(BaseHTTPRequestHandler):
         
 
     def do_POST(self):
-        pass
+        size = self.headers.get('Content-Length')
+        data = self.rfile.read(int(size))
+        print(data)
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        client_socket.sendto(data, (SOCKET_HOST, SOCKET_PORT))
+        client_socket.close()
+        self.send_response(302)
+        self.send_header('Location', '/contact')
+        self.end_headers()
 
     def send_html(self, filename, status_code=200):
         self.send_response(status_code)
@@ -40,19 +53,7 @@ class Homeworkfork(BaseHTTPRequestHandler):
         with open(filename, 'rb') as file:
             self.wfile.write(file.read( ))
 
-    def render_template(self, filename, status_code=200):
-        self.send_response(status_code)
-        self.send_header(keyword='Content-Type', value='text/html')
-        self.end_headers()
-
-
-        with open('storage/db.jsin', 'r', encoding='Utf-8') as file:
-            data = json.load(file)
-
-        template = jinja.get_template(filename)
-        html = template.render(massages=data)
-        self.wfile.write(html.encode())
-
+    
     def send_static(self, filename, status_code=200):
         self.send_response(status_code)
         mime_type, *_ = mimetypes.guess_type(filename)
@@ -64,16 +65,51 @@ class Homeworkfork(BaseHTTPRequestHandler):
         self.end_headers()
         with open(filename, 'rb') as file:
             self.wfile.write(file.read( ))
+            
+def save_data_from_form(data):
+    parse_data = urllib.parse.unquote_plus(data.decode())
+    print(parse_data)
+    try:
+        parse_dict = {key: value for key, value in [el.split('=') for el in parse_data.split('@')]}
+        print(parse_dict)
+        with open ('data/data.json', 'w', encoding='utf-8') as file:
+            json.dump(parse_dict, file, ensure_ascii=False, indent=4)
+    except ValueError as err:
+        logging.error(err)
+    except OSError as err:
+        logging.error(err)
 
-def run_server():
-    address = ('localhost', 3000)
+def run_socket_server(host, port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind((host, port))
+    logging.info('Starting socket server')
+    try:
+        while True:
+            msg, addres = server_socket.recvfrom(BUFFER_SIZE)
+            save_data_from_form(msg)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        server_socket.close()
+
+
+def run_http_server(host, port):
+    address = (host, port)
     http_server = HTTPServer(address, Homeworkfork)
+    logging.info('Starting http server')
     try:
         http_server.serve_forever()
     except KeyboardInterrupt:
+        pass
+    finally:
         http_server.server_close()
 
 
 if __name__ == '__main__':
-    run_server()
-    
+    logging.basicConfig(level=logging.DEBUG, format='%(thraedname)s %(message)s')
+
+    server = Thread(target=run_http_server, args=(HTTP_HOST, HTTP_PORT))
+    server.start()
+
+    server_socket = Thread(target=run_socket_server, args=(SOCKET_HOST, SOCKET_PORT))
+    server_socket.start()
