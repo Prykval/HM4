ecf0dbf0e26506e165d5fa2af086cbe6ecb6ca56
Prykval <pryvalera21@gmail.com>
2024-02-12 13:53:01 +0100

    4

--- front-init/main.py
+++ front-init/main.py
@@ -1,115 +1,123 @@
-import urllib.parse
-import logging
-import json
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import urllib.parse
 from pathlib import Path
 import mimetypes
+import logging
+from datetime import datetime
+import json
 from threading import Thread
 import socket
 
-BUFFER_SIZE = 1024
-BASE_DIR = Path()
-HTTP_PORT = 3000
-HTTP_HOST = '0.0.0.0'
-SOCKET_HOST = '127.0.0.1'
+BASE_DIR = Path() 
+PORT_HTTP = 3000
+HOST_HTTP = "0.0.0.0"
+SOCKET_HOST = "127.0.0.1"
 SOCKET_PORT = 5000
+BUFFER_SIZE = 1024
+
+
 
 class Homeworkfork(BaseHTTPRequestHandler):
 
     def do_GET(self):
         route = urllib.parse.urlparse(self.path)
         match route.path:
-            case '/':
-                self.send_html('index.html')
-            case '/Home':
-                self.send_html('Home.html')
-            case '/Send message':
-                self.send_html('message.html')
+            case "/":
+                self.send_html("index.html")
+            case "/message":
+                self.send_html("message.html")
             case _:
                 file = BASE_DIR.joinpath(route.path[1:])
                 if file.exists():
                     self.send_static(file)
                 else:
-                    self.send_html('error.html', 404)
-                    
-
-        
+                    self.send_html("error.html", 404)
 
     def do_POST(self):
-        size = self.headers.get('Content-Length')
+        size = self.headers.get("Content-Length")
         data = self.rfile.read(int(size))
-        print(data)
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         client_socket.sendto(data, (SOCKET_HOST, SOCKET_PORT))
         client_socket.close()
         self.send_response(302)
-        self.send_header('Location', '/contact')
+        self.send_header('Location', '/')
         self.end_headers()
+
 
     def send_html(self, filename, status_code=200):
         self.send_response(status_code)
-        self.send_header(keyword='Content-Type', value='text/html')
+        self.send_header("Content-Type", "text/html")
         self.end_headers()
-        with open(filename, 'rb') as file:
-            self.wfile.write(file.read( ))
 
-    
+        with open(filename, "rb") as file:
+            self.wfile.write(file.read())
+
     def send_static(self, filename, status_code=200):
         self.send_response(status_code)
         mime_type, *_ = mimetypes.guess_type(filename)
-    
         if mime_type:
-            self.send_header('Content-Type', mime_type)
+            self.send_header("Content-Type", mime_type)
         else:
-            self.send_header(keyword='Content-Type', value='text/plain')
+            self.send_header("Content-Type", "text/plain")
         self.end_headers()
-        with open(filename, 'rb') as file:
-            self.wfile.write(file.read( ))
-            
-def save_data_from_form(data):
-    parse_data = urllib.parse.unquote_plus(data.decode())
-    print(parse_data)
-    try:
-        parse_dict = {key: value for key, value in [el.split('=') for el in parse_data.split('@')]}
-        print(parse_dict)
-        with open ('data/data.json', 'w', encoding='utf-8') as file:
-            json.dump(parse_dict, file, ensure_ascii=False, indent=4)
-    except ValueError as err:
-        logging.error(err)
-    except OSError as err:
-        logging.error(err)
+        with open(filename, "rb", ) as file:
+            self.wfile.write(file.read())
+
 
 def run_socket_server(host, port):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind((host, port))
-    logging.info('Starting socket server')
+    logging.info("Starting socket")
     try:
         while True:
-            msg, addres = server_socket.recvfrom(BUFFER_SIZE)
-            save_data_from_form(msg)
+            msg, address = server_socket.recvfrom(BUFFER_SIZE)
+            logging.info("++")
+            save_data(msg)
     except KeyboardInterrupt:
-        pass
-    finally:
         server_socket.close()
+
 
 
 def run_http_server(host, port):
     address = (host, port)
     http_server = HTTPServer(address, Homeworkfork)
-    logging.info('Starting http server')
+    logging.info("Starting http")
     try:
         http_server.serve_forever()
     except KeyboardInterrupt:
-        pass
-    finally:
         http_server.server_close()
 
+def save_data(data):
+    data_parse = urllib.parse.unquote_plus(data.decode())
+    try:
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+        new_data = {current_time: {key: value for key, value in [el.split('=') for el in data_parse.split('&')]}}
 
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG, format='%(thraedname)s %(message)s')
+        file_path = "storage/data.json"
 
-    server = Thread(target=run_http_server, args=(HTTP_HOST, HTTP_PORT))
-    server.start()
+        try:
+            with open(file_path, "r", encoding="utf-8") as file:
+                existing_data = json.load(file)
+        except FileNotFoundError:
+            existing_data = {}
+        except ValueError:
+            existing_data = {}
+        existing_data.update(new_data)
 
-    server_socket = Thread(target=run_socket_server, args=(SOCKET_HOST, SOCKET_PORT))
-    server_socket.start()
+        with open(file_path, "w", encoding="utf-8") as file:
+            json.dump(existing_data, file, ensure_ascii=False, indent=2)
+
+    except ValueError as error:
+        logging.error(f"ValueError: {error}")
+    except OSError as oser:
+        logging.error(f"OSError: {oser}")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG, format="%(threadName)s%(message)s")
+
+    ser_http = Thread(target=run_http_server, args=(HOST_HTTP, PORT_HTTP))
+    ser_http.start()
+
+    ser_socker = Thread(target=run_socket_server, args=(SOCKET_HOST, SOCKET_PORT))
+    ser_socker.start()
